f5977a9335801a92e2d273ca746e5827e9d6fe01
Nina Yujiri <ninayujiri@gmail.com>
2018-04-25 23:58:11 +0000

    added loadTweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,93 +6,126 @@
 
 $(document).ready(function() {
 
-// Database
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+  // Database
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
       },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
+      "created_at": 1461116232227
     },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
       },
-      "handle": "@johann49"
+      "created_at": 1461113959088
     },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
 
 
-// Append each Tweet to the Container
-function renderTweets(tweets) {
-  for (let currentTweet of tweets) {
-    $('#tweets').append(createTweetElement(currentTweet));
-  }
-};
+  // Append each Tweet to the Container
+  function renderTweets(tweets) {
+    $('#tweets').empty();
+    for (let currentTweet of tweets) {
+      $('#tweets').append(createTweetElement(currentTweet));
+    }
+  };
 
 
-// Creates a single tweet
-function createTweetElement(tweet) {
+  // Creates a single tweet
+  function createTweetElement(tweet) {
 
-  let $tweet = $('<article>').addClass('tweet')
-    const $header = $("<header>");
-    const $footer = $("<footer>");
-    const $span = $("<span>").addClass("icons");
+    let $tweet = $('<article>').addClass('tweet')
+      const $header = $("<header>");
+      const $footer = $("<footer>");
+      const $span = $("<span>").addClass("icons");
 
-    $("<img>").attr('src', tweet.user.avatars.small).appendTo($header);
-    $("<p>").addClass("handle").text(tweet.user.handle).appendTo($header);
-    $("<h2>").addClass("name").text(tweet.user.name).appendTo($header);
+      $("<img>").attr('src', tweet.user.avatars.small).appendTo($header);
+      $("<p>").addClass("handle").text(tweet.user.handle).appendTo($header);
+      $("<h2>").addClass("name").text(tweet.user.name).appendTo($header);
 
-    $tweet.append($header);
+      $tweet.append($header);
 
-    $("<h4>").text(tweet.content.text).appendTo($tweet);
+      $("<h4>").text(tweet.content.text).appendTo($tweet);
 
-    $("<i>").addClass("fas fa-heart").appendTo($span);
-    $("<i>").addClass("fas fa-retweet").appendTo($span);
-    $("<i>").addClass("fas fa-flag").appendTo($span);
+      $("<i>").addClass("fas fa-heart").appendTo($span);
+      $("<i>").addClass("fas fa-retweet").appendTo($span);
+      $("<i>").addClass("fas fa-flag").appendTo($span);
 
-    $footer.append($span);
+      $footer.append($span);
 
-    $("<p>").addClass("time-stamp").text(tweet.created_at).appendTo($footer);
+      $("<p>").addClass("time-stamp").text(tweet.created_at).appendTo($footer);
 
-    $tweet.append($footer);
+      $tweet.append($footer);
 
-  return $tweet;
-};
+    return $tweet;
+  };
 
-renderTweets(data);
+
+  // AJAX request
+  $('form').on('submit', function (event) {
+      event.preventDefault();
+      const data = $( this ).serialize();
+
+      $.ajax({
+        url: '/tweets/',
+        method: 'POST',
+        data: data
+
+      })
+      .done(function () {
+        loadTweets();
+      })
+      .fail(function () {
+        alert('request failed');
+      });
+  });
+
+
+  // Gets tweets from the Tweets page
+  function loadTweets() {
+    $.ajax({
+      url: '/tweets/',
+      method: 'GET',
+      success: function(data) {
+        renderTweets(data.reverse());
+      }
+    })
+  };
+
+  loadTweets();
 
 });
 
