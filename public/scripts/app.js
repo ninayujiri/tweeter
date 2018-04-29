b7c6f21a9af05a495ee48842d81bddc51d57db34
Nina Yujiri <ninayujiri@gmail.com>
2018-04-29 04:04:04 +0000

    made JS more modular

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
 
-  // Focuses on the text area as soon as the page loads
-  $("textarea").focus();
-
   // Appends each Tweet to the Container
   function renderTweets(tweets) {
     $('#tweets').empty();
@@ -11,56 +8,56 @@ $(document).ready(function() {
     }
   };
 
+  // Produces and fades out error messages
+  function errorMsg() {
+    setTimeout(function() { $(".new-tweet .error").fadeOut(1000); }, 2000)
+    $(".new-tweet .error").show();
+    setTimeout(function() { $(".new-tweet .error").fadeOut(1000); }, 2000)
+  };
+
   // Creates a single tweet
   function createTweetElement(tweet) {
-    let $tweet = $('<article>').addClass('tweet')
+    let $tweet = $("<article>").addClass('tweet')
     const $header = $("<header>");
     const $footer = $("<footer>");
     const $span   = $("<span>").addClass("icons");
+    const $timeStamp = moment.utc(tweet.created_at).fromNow();
 
+    // Tweet header
     $("<img>").attr('src', tweet.user.avatars.small).appendTo($header);
     $("<p>").addClass("handle").text(tweet.user.handle).appendTo($header);
     $("<h2>").addClass("name").text(tweet.user.name).appendTo($header);
-
     $tweet.append($header);
 
+    // Tweet body
     $("<h4>").text(tweet.content.text).appendTo($tweet);
 
+    // Tweet footer
     $("<i>").addClass("fas fa-heart").appendTo($span);
     $("<i>").addClass("fas fa-retweet").appendTo($span);
     $("<i>").addClass("fas fa-flag").appendTo($span);
 
     $footer.append($span);
 
-    $("<p>").addClass("time-stamp").text(moment.utc(tweet.created_at).fromNow()).appendTo($footer);
+    $("<p>").addClass("time-stamp").text($timeStamp).appendTo($footer);
 
     $tweet.append($footer);
     return $tweet;
   };
 
   $('form').on('submit', function (event) {
-    event.preventDefault();
     const data = $(this).serialize();
     const tweetContent = ($.trim($(this.text).val()));
 
     // Checks to make sure tweet form is not empty or over 140 characters
     if (tweetContent.length === 0) {
-      $(function() {
-        setTimeout(function() { $(".new-tweet .error").fadeOut(1500); }, 2500)
-        $(".new-tweet .error").show();
-        setTimeout(function() { $(".new-tweet .error").fadeOut(1500); }, 2500)
-      });
       var $errMsg = "You submitted an empty tweet! Try again.";
+      errorMsg($errMsg);
     }
 
     if (tweetContent.length > 140) {
-      $(function() {
-        setTimeout(function() { $(".new-tweet .error").fadeOut(1500); }, 2500)
-        $(".new-tweet .error").show();
-        setTimeout(function() { $(".new-tweet .error").fadeOut(1500); }, 2500)
-      });
-
       var $errMsg = "Your tweet is too long!";
+      errorMsg($errMsg);
       event.preventDefault();
       return $(".new-tweet .error").text($errMsg);
     }
@@ -79,13 +76,13 @@ $(document).ready(function() {
       $('.counter').text(140);
     })
     .fail(function () {
-    $(".new-tweet .error").text($errMsg);
+      $(".new-tweet .error").text($errMsg);
     })
   });
 
   // Toggle function when Compose button is clicked
-  $( "#compose-button" ).click(function() {
-    $( ".new-tweet" ).slideToggle();
+  $("#compose-button").click(function() {
+    $(".new-tweet").slideToggle();
     $("textarea").focus();
   });
 
@@ -102,6 +99,9 @@ $(document).ready(function() {
 
   // Loads the tweets as soon as user lands on the page
   loadTweets();
+
+  // Focuses on the text area as soon as the page loads
+  $("textarea").focus();
 
 });
 
